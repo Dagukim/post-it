12e867ef8bee20d75dfdaff625bccba32e693ec4
Dagukim <ikaros6388@gmail.com>
2024-04-13 23:53:50 +0900

    change memo style

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Memo from '@/memo';
 
 const App: React.FC = () => {
 	const [memos, setMemos] = useState<number[]>([1, 2, 3]);
 
-	const [defaultPosition, setDefaultPosition] = useState<{
-		x: number;
-		y: number;
-	}>({ x: 0, y: 0 });
-
-	useEffect(() => {
-		const updatePosition = () => {
-			const x = window.innerWidth / 2 - 50;
-			const y = window.innerHeight / 2 - 50;
-			setDefaultPosition({ x, y });
-		};
-		updatePosition();
-
-		window.addEventListener('resize', updatePosition);
-
-		return () => window.removeEventListener('resize', updatePosition);
-	}, []);
-
 	const handleMemoClick = (index: number) => {
 		const newMemos = [...memos];
-		const clickedMemo = newMemos.splice(index, 1);
-		newMemos.push(clickedMemo[0]);
+		const clickedMemo = newMemos.splice(index, 1)[0];
+		newMemos.push(clickedMemo);
 		setMemos(newMemos);
 	};
 
 	return (
-		<div className="relative w-full h-screen">
+		<div className="relative w-full h-screen overflow-hidden bg-[#f3e5c9]">
 			{memos.map((key, index) => (
-				<Memo
-					key={key}
-					defaultPosition={defaultPosition}
-					onClick={() => handleMemoClick(index)}
-				/>
+				<Memo key={key} onMouseDown={() => handleMemoClick(index)} />
 			))}
 		</div>
 	);
--- src/memo.tsx
+++ src/memo.tsx
@@ -1,16 +1,18 @@
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRef, useState } from 'react';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 
 interface MemoProps {
-	defaultPosition: { x: number; y: number };
-	onClick: () => void;
+	onMouseDown: () => void;
 }
 
-const Memo: React.FC<MemoProps> = ({ defaultPosition, onClick }) => {
-	const nodeRef = useRef(null);
-	const [position, setPosition] = useState<{ x: number; y: number }>(
-		defaultPosition
-	);
+const Memo: React.FC<MemoProps> = ({ onMouseDown }) => {
+	const nodeRef = useRef<HTMLDivElement>(null);
+	const [position, setPosition] = useState<{ x: number; y: number }>({
+		x: 0,
+		y: 0
+	});
 
 	const handleDrag = (_e: DraggableEvent, data: DraggableData) => {
 		const { x, y } = data;
@@ -20,20 +22,41 @@ const Memo: React.FC<MemoProps> = ({ defaultPosition, onClick }) => {
 	return (
 		<Draggable
 			nodeRef={nodeRef}
-			defaultPosition={defaultPosition}
 			onStop={handleDrag}
-			onMouseDown={onClick}
+			onMouseDown={onMouseDown}
 			handle="#handle"
+			bounds="parent"
 		>
 			<div
 				ref={nodeRef}
-				className="absolute bg-yellow-100 border border-black group"
+				className="absolute overflow-x-auto overflow-y-hidden bg-yellow-100 shadow-md resize max-w-80 min-w-48 min-h-28 group"
 			>
 				<div
 					id="handle"
-					className="transition-all duration-100 cursor-move bg-amber-300 group-hover:py-2"
-				></div>
-				<div className="p-2">
+					className="flex justify-between p-2 cursor-move bg-amber-300"
+				>
+					<button
+						className="transition-all duration-300 opacity-0 group-hover:opacity-100"
+						aria-label="edit"
+					>
+						<FontAwesomeIcon
+							className="block"
+							icon={faPen}
+							size="sm"
+						/>
+					</button>
+					<button
+						className="transition-all duration-300 opacity-0 group-hover:opacity-100"
+						aria-label="delete"
+					>
+						<FontAwesomeIcon
+							className="block"
+							icon={faTrash}
+							size="sm"
+						/>
+					</button>
+				</div>
+				<div className="p-2 break-words whitespace-pre-wrap">
 					<h2>title</h2>
 					<p>description</p>
 				</div>
