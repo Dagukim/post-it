119f603a19d49aeca23a470f9ceb89cbf6b71511
Dagukim <ikaros6388@gmail.com>
2024-04-21 22:03:04 +0900

    deploy

--- src/components/TextEditArea.tsx
+++ src/components/TextEditArea.tsx
@@ -4,7 +4,7 @@ import { TextEditAreaProps } from '@/types';
 const TextEditArea: React.FC<TextEditAreaProps> = ({ onBlur, text }) => {
 	return (
 		<ReactTextareaAutosize
-			className="flex-1 w-full p-2 overflow-auto bg-transparent outline-none resize-none"
+			className="flex-grow w-full p-2 overflow-auto bg-transparent outline-none resize-none"
 			onBlur={(e) => onBlur(e.target.value)}
 			cols={50}
 			placeholder="Type your memo here..."
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/',
+	base: '/post-it/',
 	plugins: [react()],
 	resolve: {
 		alias: {
